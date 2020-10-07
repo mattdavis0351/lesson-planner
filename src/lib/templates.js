f823a19ea1dd45df1d712a26958f3a3dca4d638a
mattdavis0351 <mattdavis0351@github.com>
2020-10-06 21:20:44 -0700

    wtf

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -14,7 +14,7 @@ async function populateTemplateFiles(
     `${templateDir}/v${templateVersion}`
   );
 
-  // nj.configure(path.resolve(`${templateDir}/v${templateVersion}`));
+  nj.configure(__dirname + `dist/templates/v${templateVersion}`);
   for (let i = 0; i < templateFiles.length; i++) {
     console.log("using loop and nunjucks to populate templates");
     console.log("creating object keys with .replace()");
@@ -32,7 +32,7 @@ async function populateTemplateFiles(
     // path.resolve(path.dirname(__dirname),'dist','templates',`v${templateVersion}`,templateFiles[i])
 
     const newContent = nj.render(
-      "./dist/templates/v1/" + templateFiles[i],
+      templateFiles[i],
       // path.resolve(
       //   path.dirname(__dirname),
       //   "src",
