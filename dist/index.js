3ab9e5ff2055df03353d83fcb80afc4b7ee5affc
mattdavis0351 <mattdavis0351@github.com>
2020-10-07 11:23:49 -0700

    trying render string

--- dist/index.js
+++ dist/index.js
@@ -204,26 +204,25 @@ async function populateTemplateFiles(
     `${templateDir}/v${templateVersion}`
   );
 
-  // nj.configure(__dirname + `dist/templates/v${templateVersion}`);
   for (let i = 0; i < templateFiles.length; i++) {
-    console.log("using loop and nunjucks to populate templates");
-    console.log("creating object keys with .replace()");
     const objectKey = templateFiles[i].replace(".", "");
     console.log("replace sucessful");
     console.log("current file nj wants to render " + templateFiles[i]);
-    console.log(
-      `path is currently: ${templateDir}/v${templateVersion}/${templateFiles[i]}`
+    // console.log(
+    //   `path is currently: ${templateDir}/v${templateVersion}/${templateFiles[i]}`
+    // );
+    const contents = fs.readFileSync(
+      `${templateDir}/v${templateVersion}/${templateFiles[i]}`
     );
-    const contents = fs.readdirSync(`${templateDir}/v${templateVersion}`);
     // /v${templateVersion}/${templateFiles[i]}
-    console.log(`dir contents is : ${contents}`);
+    console.log(`file contents is : ${contents}`);
 
-    console.log(path.dirname(__dirname));
+    // console.log(path.dirname(__dirname));
     // path.resolve(path.dirname(__dirname),'dist','templates',`v${templateVersion}`,templateFiles[i])
 
-    const newContent = nj.render(
+    const newContent = nj.renderString(
       // path.resolve(
-      `../${templateDir}/v${templateVersion}/${templateFiles[i]}`,
+      contents,
       {
         certificationName,
         objs,
