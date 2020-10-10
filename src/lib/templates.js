db895bbb27389fe9b3ef3e59de61083fa2b7b91c
mattdavis0351 <mattdavis0351@github.com>
2020-10-10 12:52:38 -0700

    refactor template.js

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -7,26 +7,41 @@ function populateTemplateFiles(
   objs,
   templateDir
 ) {
-  let filesToWrite = {};
+  // let filesToWrite = {};
+
+  // const templateFiles = fs.readdirSync(`${templateDir}/v${templateVersion}`);
+
+  // for (let i = 0; i < templateFiles.length; i++) {
+  //   const objectKey = templateFiles[i].replace(".", "").toLowerCase();
+  //   const contents = fs.readFileSync(
+  //     `${templateDir}/v${templateVersion}/${templateFiles[i]}`
+  //   );
+  //   console.log("nj currently templating " + templateFiles[i]);
+  //   const newContent = nj.renderString(contents.toString(), {
+  //     certificationName,
+  //     objs,
+  //   });
+
+  //   filesToWrite[objectKey] = newContent;
+  // }
+  // console.log("nj object to return is");
+  // console.log(filesToWrite);
+  // return filesToWrite;
 
   const templateFiles = fs.readdirSync(`${templateDir}/v${templateVersion}`);
-
-  for (let i = 0; i < templateFiles.length; i++) {
-    const objectKey = templateFiles[i].replace(".", "").toLowerCase();
-    const contents = fs.readFileSync(
-      `${templateDir}/v${templateVersion}/${templateFiles[i]}`
-    );
-    console.log("nj currently templating " + templateFiles[i]);
-    const newContent = nj.renderString(contents.toString(), {
+  const filesToWrite = templateFiles.map((file) => {
+    const contents = fs
+      .readFileSync("./src/lib/templates/v1/" + file)
+      .toString();
+    const objKey = file.replace(".", "").toLowerCase();
+    const newContents = nj.renderString(contents, {
       certificationName,
       objs,
     });
+    return { [objKey]: newContents };
+  });
 
-    filesToWrite[objectKey] = newContent;
-  }
-  console.log("nj object to return is");
-  console.log(filesToWrite);
-  return filesToWrite;
+  return Object.assign({}, ...filesToWrite);
 }
 
 module.exports = { populateTemplateFiles };
